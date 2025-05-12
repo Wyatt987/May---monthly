8bd0f6ad91ea28b055ba106e66be3be87cb864a6
wyatt987 <526wbedor@frhsd.com>
2025-05-12 10:42:36 -0400

    first commit

---
+++ players.js
@@ -0,0 +1,37 @@
+const players = [
+    {
+        firstName: 'Jalen',
+        lastName: 'Brunson',
+        position: 'PG',
+        age: 26,
+        photo: 'https://picsum.photos/200',
+    } ,
+    {
+        firstName: 'Mikal',
+        lastName: 'Bridges',
+        position: 'SF',
+        age: 28,
+        photo: 'https://picsum.photos/200',
+    },
+    {
+        firstName: 'KAT',
+        lastName: 'Anthony-Towns',
+        position: 'PF',
+        age: 29,
+        photo: 'https://picsum.photos/200',
+    },
+    {
+        firstName: 'Mitchell',
+        lastName: 'Robinson',
+        position: 'C',
+        age: 25,
+        photo: 'https://picsum.photos/200',
+    },
+    {
+        firstName: 'OG',
+        lastName: 'Anunoby',
+        position: 'SF',
+        age: 27,
+        photo: 'https://picsum.photos/200',
+    }
+]
